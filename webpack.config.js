e277abfdbcc8bf5f371a1d5f9cb6df3ed3e5213c
Eriksson Monteiro <erikssonmonteiro@gmail.com>
2022-10-21 02:07:13 +0100

    update sqlite3 distribution

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
-const path              = require('path');
-
+const path       = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    target   : 'node-webkit',
+    target   : 'node',
     mode     : 'development',
     entry    : './src/index.js',
-    externals: {
-        sqlite3: 'commonjs sqlite3',
-    },
     output   : {
         filename: 'tangled-advertisement.js',
         path    : path.resolve(__dirname, 'lib')
+    },
+    resolve: {
+        alias: {
+            [path.join(__dirname, 'node_modules/sqlite3/lib/sqlite3-binding.js')]: path.join(__dirname, 'src/database/sqlite3/sqlite3-binding.js')
+        }
     },
     devtool  : 'inline-source-map',
     module   : {
@@ -37,5 +39,15 @@ module.exports = {
                 type: 'asset/source',
             }
         ]
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'node_modules/sqlite3/lib/binding/**/node_sqlite3.node',
+                    to  : 'build/node_sqlite3.node'
+                }
+            ]
+        })
+    ]
 };
